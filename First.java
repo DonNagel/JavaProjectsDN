1ee9ec23b107a4e4411fd3975b5020e05ad1d2b9
DonNagel <donovanadamga@gmail.com>
2025-01-16 18:42:35 -0500

    First Commit: Attempt at first java

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job Title:Software Developer");
+        System.out.println("Why: It's the closest to the work I want to do in the Audio Technology field, developing programs like Repeaper, Protools, Musecore, and Sibelius.");
+        System.out.println("Median Salary: $130,160");
+    }
+}
