9a5869a5ca95e089ca2c799e1379bb6c3e61cb5f
DonNagel <donovanadamga@gmail.com>
2025-02-09 16:58:36 -0500

    Pet Project M3A1 commit, plus runner file for it.

--- Pet.java
+++ Pet.java
@@ -3,6 +3,7 @@ public class Pet {
     private String name;
     private String species;
     private int age;
+    private String noise;
 
     //public Constructor(s)
     public Pet()
@@ -11,7 +12,6 @@ public class Pet {
         this.setSpecies("");
         this.setAge(0);
     }
-
     //public set or mutator method for every private data field (except when the data field is read only.)
     public void setName(String newName)
     {
@@ -25,7 +25,32 @@ public class Pet {
     {
         this.age = newAge;
     }
+    /*After a lot of testing and attempts, I got a version of this to work as per requested in the assignment. 
+    Essentially, I learned how 'this' works in the class file, and how 'this' refers to the class name, so all of the 'this' in this file == Pet.
+    With that knowledge I now understand better what all the this.___ means and I was able to construct an if else tree to set the noise for 4 animals.
+    I included Opossum because of your youtube profile picture.
 
+    Each if statement checks if the string in species is equal to an expected string, if it is then noise is set to a predetermined string that the animal makes.
+    
+    After doing some digging around, I discovered that '==' does not work for strings, and you need to use .equals()
+    */
+    public void setNoise()
+    {
+        if (this.species.equals( "Cat")){
+            this.noise = "Meow";
+        }else if (this.species.equals( "Dog")){
+            this.noise = "Woof";
+        }else if (this.species.equals( "Bird")){
+            this.noise = "Tweet";
+        }else if (this.species.equals( "Opossum")){
+            this.noise = "Hiss";
+        // A special example else if so the example text works for the user.
+        }else if (this.species.equals("Species")){
+            this.noise = "Noise the animal makes.";
+        }else {
+            this.noise = "Species sound unknown.";
+        }
+    }
     //public get or accessor method for every private data field (except i it is a secret)
     public String getName()
     {
@@ -39,6 +64,10 @@ public class Pet {
     {
         return this.age;
     }
+    public String getNoise()
+    {
+        return this.noise;
+    }
     /*public toString method that prints out all of the object state
     (meaning the data in the object instane that you will later instantiate in the main methos)
      */
@@ -49,6 +78,8 @@ public class Pet {
         output += this.getName();
         output += "\nSpecies: ";
         output += this.species;
+        output += "\nNoise: ";
+        output += this.noise;
         output += "\nAge: ";
         output += this.age;
         return output;
